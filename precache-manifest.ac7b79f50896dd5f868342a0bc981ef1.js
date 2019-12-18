691c33713755453ba3ba15b396125ffd2890a492
Aritra Chatterjee <aritrachatterjee07@gmail.com>
2019-12-18 22:04:59 +0530

    Updates

---
+++ precache-manifest.ac7b79f50896dd5f868342a0bc981ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7512996676c2081b1c6ce12ee94d3584",
+    "url": "/lottery-blockchain/index.html"
+  },
+  {
+    "revision": "fee502eb07d4ac7efa71",
+    "url": "/lottery-blockchain/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0e0f66dbdf94e860eeba",
+    "url": "/lottery-blockchain/static/js/2.dc3bf76a.chunk.js"
+  },
+  {
+    "revision": "421bd247d27199660986f750cdf8b760",
+    "url": "/lottery-blockchain/static/js/2.dc3bf76a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fee502eb07d4ac7efa71",
+    "url": "/lottery-blockchain/static/js/main.750874e2.chunk.js"
+  },
+  {
+    "revision": "624d199a3b820ba80ce6",
+    "url": "/lottery-blockchain/static/js/runtime-main.9b8601dc.js"
+  }
+]);
